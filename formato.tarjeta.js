8fdaea507c485f55744d9deb982830bd8b1b5307
pcalle4 <pcalle4@gmail.com>
2022-04-01 15:01:24 -0500

    Add files via upload

---
+++ formato.tarjeta.js
@@ -0,0 +1,16 @@
+var wpwlOptions = {
+    style: 'card',
+    locale: 'es',
+    labels: {cvv: 'CVV', cardHolder: 'Nombre (Igual que en la tarjeta)'},
+    onBeforeSubmitCard: function() {
+      if($('.wpwl-control-cardHolder').val() === '') {
+        $('.wpwl-control-cardHolder').addClass("wpwl-has-error");
+        $('.wpwl-control-cardHolder').after("<div class='wpwl-hint-cardHolderError'>Campo requerido</div>");
+	$('.wpwl-button-pay').addClass("wpwl-button-error").attr('disabled', 'disabled');
+	return false;
+      } else {
+	return true;
+    console.log(PAGADO)
+      }
+    }
+  }
